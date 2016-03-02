f6a874e89004bb3c82f4461d070b5679e2fed94b
BrotherBeard <littlebeard@bjtu.edu.cn>
2016-03-02 17:07:37 +0800

    add necessary annotations

--- addLoadEvent.js
+++ addLoadEvent.js
@@ -1,26 +1,28 @@
 /**
-* func : bind multi-functions to the window.onload
-*
+* @usage: bind multiple functions to the window.onload event
+* 
 */
 
 function addLoadEvent (func) {
-  var oldOnLoad = window.onload;
-  if (typeof oldOnLoad !== 'function') {
-    window.onload = func;
-  } 
-  else {
-    window.onload = function () {
-      oldOnLoad();
-      func();
-  }
+    var oldOnLoad = window.onload;
+    if (typeof oldOnLoad !== 'function') {
+        window.onload = func;
+    } 
+    else {
+        window.onload = function () {
+            oldOnLoad();
+            func();
+        }
+    }
 }
 
 function a () {
-  console.log('test a');
-  }
+    console.log('this is func a');
+}
 function b () {
-  console.log('test b');
-  }
-  
+    console.log('this is func b');
+}
+  //bind the function a, b
   addLoadEvent(a);
   addLoadEvent(b);
+  //... bind more functions here
