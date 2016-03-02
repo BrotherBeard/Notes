824b14a144ab245200d54fdf08c9a82a1f49ebf9
BrotherBeard <littlebeard@bjtu.edu.cn>
2016-03-02 17:14:01 +0800

    adjust the code indentation

--- addLoadEvent.js
+++ addLoadEvent.js
@@ -1,5 +1,5 @@
 /**
-* @usage: bind multiple functions to the window.onload event
+* @usage: bind multiple functions to the window.onload event 
 * 
 */
 
@@ -7,22 +7,24 @@ function addLoadEvent (func) {
     var oldOnLoad = window.onload;
     if (typeof oldOnLoad !== 'function') {
         window.onload = func;
-    } 
-    else {
+    } else {
         window.onload = function () {
             oldOnLoad();
             func();
-        }
+        };
     }
 }
 
 function a () {
     console.log('this is func a');
 }
+
 function b () {
     console.log('this is func b');
 }
-  //bind the function a, b
-  addLoadEvent(a);
-  addLoadEvent(b);
-  //... bind more functions here
+
+//bind the function a, b
+addLoadEvent(a);
+addLoadEvent(b);
+
+//bind more functions here ...
